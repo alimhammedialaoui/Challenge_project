8d8eee2701a6f40c3a7618c33306c0395c4142f6
alilou1998 <alimalaoui3@gmail.com>
2019-10-01 16:51:11 +0100

    Commit

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Challenge_project
 I'm using Git to commit my work on GitHub.
 Notice:Watch this: https://www.youtube.com/watch?v=jt2qlQ-P30E
+Read this: https://www.tutorielsenfolie.com/tutoriels-40-formulaire-dynamique.html
 Alilou1998 Copyright
--- templates/pages/quotidien.js
+++ templates/pages/quotidien.js
@@ -1,6 +1,14 @@
-function checked(){
+function check(){
     var type=document.getElementById("radio_btn");
     if (type.checked==true){
-        document.write();
+        document.write(
+            <fieldset>
+                        <label>Ville:</label>
+                        <input type="text" name="texte" id="trajet1"/><br/>
+                        <label>Destination:</label>
+                        <input type="text" name="texte" id="trajet2"/>
+            </fieldset>
+        );
+        alert("oui checked");
     }
 }
