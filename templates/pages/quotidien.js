7bea18aa4b404a24eeebdeab93601a3cd6652878
alilou1998 <alimalaoui3@gmail.com>
2019-10-01 18:01:36 +0100

    Commit

--- templates/pages/quotidien.js
+++ templates/pages/quotidien.js
@@ -1,14 +1,48 @@
-function check(){
-    var type=document.getElementById("radio_btn");
-    if (type.checked==true){
-        document.write(
-            <fieldset>
-                        <label>Ville:</label>
-                        <input type="text" name="texte" id="trajet1"/><br/>
-                        <label>Destination:</label>
-                        <input type="text" name="texte" id="trajet2"/>
-            </fieldset>
-        );
-        alert("oui checked");
+function ajout(element){
+    var type = document.getElementById("radio_btn");
+    if(type.checked){
+        var formulaire = window.document.formulaireDynamique;
+        // On clone le bouton d'ajout
+        var ajout = element.cloneNode(true);
+        // Crée un nouvel élément de type "input"
+        var champ1 = document.createElement("input");
+        var label1 = document.createElement("label");
+        var champ2 = document.createElement("input");
+        var label2 = document.createElement("label");
+        // Les valeurs encodée dans le formulaire seront stockées dans un tableau
+        champ1.name = "champs[]";
+        champ1.type = "text";
+        label1.innerText="Ville:";
+        
+        champ2.name="champs[]";
+        champ2.type="type";
+        label2.innerText="\n Destination:";
+            
+        var sup = document.createElement("input");
+        
+        
+        // Ajout de l'événement onclick
+        
+        
+        // On crée un nouvel élément de type "p" et on insère le champ l'intérieur.
+        var bloc = document.createElement("p");
+        bloc.appendChild(label1);
+        bloc.appendChild(champ1);
+        
+        bloc.appendChild(label2);
+        bloc.appendChild(champ2);
+        formulaire.insertBefore(ajout, element);
+        
+        formulaire.insertBefore(bloc, element);
     }
+ }
+ function suppression(element){
+ var formulaire = window.document.formulaireDynamique;
+      
+ // Supprime le bouton d'ajout
+ formulaire.removeChild(element.previousSibling);
+ // Supprime le champ
+ formulaire.removeChild(element.nextSibling);
+ // Supprime le bouton de suppression
+ formulaire.removeChild(element);
 }
